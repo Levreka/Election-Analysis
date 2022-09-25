c74cf7523dd8af63bd1e928664a5c29947e19c67
Franklin Tello <franklintello@msn.com>
2022-09-24 19:19:26 -0700

    Practice code updated with comments

--- Election_Analysis/Python_practice.py
+++ Election_Analysis/Python_practice.py
@@ -2,35 +2,60 @@ print("Hello World")
 counties = ["Arapahoe","Denver","Jefferson"]
 if counties[1] == 'Denver':
     print(counties[1])
-counties = ["Arapahoe","Denver","Jefferson"]
+# This code was comment out because variable holding the list 
+# was created on line 2 of the code
+# code will run normal it doesn't required the code on line 9
+# it does run normal even with that code not comment out
+# counties = ["Arapahoe","Denver","Jefferson"]
+
+# line of code shows how to use membership operators in and not in
 if "El Paso" in counties:
     print("El Paso is in the list of counties.")
 else:
     print("El Paso is not the list of counties.")
 
+
+# this line of code is combining membership operators 
+# and logical operators into one code 20-23 lines 
+# are using "and" logical operators "in" membership operators 
 if "Arapahoe" in counties and "El Paso" in counties:
     print("Arapahoe and El Paso are in the list of counties.")
 else:
     print("Arapahoe or El Paso is not in the list of counties.")
 
+# line 27-30 are using logical operator "or" membership operator "in"
 if "Arapahoe" in counties or "El Paso" in counties:
     print("Arapahoe or El Paso is in the list of counties.")
 else:
     print("Arapahoe and El Paso are not in the list of counties.")
 
+# line 33-37 are using logical operator "or" 
+# membership operator "in" also "not in"
 if "Arapahoe" in counties and "El Paso" not in counties:
    print("Only Arapahoe is in the list of counties.")
 else:
     print("Arapahoe is in the list of counties and El Paso is not in the list of counties.")
+
+# practicing for loop also know as count-controlled loop
+# it will continue to loop throught the counties 
+# until it has no more items to loop over
 for county in counties:
     print(county)
+# creating a new variable that holds a list
 numbers = [0, 1, 2, 3, 4]
+# starting the for loop to print all numbers in our list
 for num in numbers:
     print(num)
+# for loop using range function to print all the numbers in list
 for num in range(5):
     print(num)
+# The variable i is used to indicate the index, or the values 0, 1, and 2, in the length of the counties list. The letter i is often used for simplicity, but any variable can be used.
+# Inside the range() function, we get the length of the list of counties, which is the integer 3.
+# Then, we iterate through the list, where the variable i is equal to 0 for the first index. The 0 is passed into the print(counties[i]) statement, where i = 0, and "Arapahoe" is printed.
+# This process is continued until all three items, or counties, in the list are printed to the screen.
 for i in range(len(counties)):
     print(counties[i])
+
 counties_tuples = ("Arapahoe","Denver","Jefferson") 
 for i in range(len(counties_tuples)):
     print(counties_tuples)
